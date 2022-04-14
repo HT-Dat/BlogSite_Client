acc425bfa0e978625dd436ae165ce04403d2bb68
HT-Dat <hotiendat661@gmail.com>
2022-04-15 03:57:26 +0700

    Add send reset pass

--- src/firebase.js
+++ src/firebase.js
@@ -77,3 +77,12 @@ const registerWithEmailAndPassword = async (name, email, password) => {
     alert(err.message);
   }
 };
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
