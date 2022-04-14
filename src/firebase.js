40785ef3ba73cc088873c0f922c1f6bdb28f0835
HT-Dat <hotiendat661@gmail.com>
2022-04-15 03:56:40 +0700

    add register with email pass

--- src/firebase.js
+++ src/firebase.js
@@ -62,3 +62,18 @@ const logInWithEmailAndPassword = async (email, password) => {
     alert(err.message);
   }
 };
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
