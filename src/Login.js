7c038658f39e07fe27d0b597f180507aa0069849
HT-Dat <hotiendat661@gmail.com>
2022-04-23 14:09:04 +0700

    Add bg image, add green filter on the left of the page

--- src/Login.js
+++ src/Login.js
@@ -1,54 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { auth, signInWithEmailAndPassword, signInWithGoogle } from "./firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import "./Login.css";
+/* eslint-disable */
+import React, { useState } from "react";
+import { logInWithEmailAndPassword, signInWithGoogle } from "./firebase";
+import { Link } from "react-router-dom";
+import bg from "./assets/login-left.jpg";
+import glogo from "./assets/google.svg";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [user, loading, error] = useAuthState(auth);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (loading) {
-      // maybe trigger a loading screen
-      return;
-    }
-    if (user) navigate("/dashboard");
-  }, [user, loading]);
   return (
-    <div className="login">
-      <div className="login__container">
-        <input
-          type="text"
-          className="login__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <input
-          type="password"
-          className="login__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button
-          className="login__btn"
-          onClick={() => signInWithEmailAndPassword(email, password)}
-        >
-          Login
-        </button>
-        <button className="login__btn login__google" onClick={signInWithGoogle}>
-          Login with Google
-        </button>
-        <div>
-          <Link to="/reset">Forgot Password</Link>
-        </div>
-        <div>
-          Don't have an account? <Link to="/register">Register</Link> now.
-        </div>
-      </div>
+    <div
+      className="flex flex-row bg-no-repeat bg-cover"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="w-3/5 h-screen bg-yellow-300/75"></div>
     </div>
   );
 }
+
 export default Login;
