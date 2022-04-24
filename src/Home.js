afd27df06ff97cb7a3955c735351139185232e48
HT-Dat <hotiendat661@gmail.com>
2022-04-24 19:25:41 +0700

    Add title for portfolio

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+function Home() {
+  return (
+    <div>
+      <div className="grid grid-cols-2 h-screen">
+        <div className="">
+          <div>WEB DESIGNER & DEVELOPER</div>
+          <div>I'm Hồ Tiến Đạt</div>
+          <div>MORE ABOUT ME </div>
+        </div>
+        <div className="bg-amber-300"></div>
+      </div>
+    </div>
+  );
+}
+export default Home;
