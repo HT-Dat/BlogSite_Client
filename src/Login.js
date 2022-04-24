5b4a9e4868e279f52d3b83973d1815cfe63366b9
HT-Dat <hotiendat661@gmail.com>
2022-04-25 02:19:42 +0700

    Remove text-sm

--- src/Login.js
+++ src/Login.js
@@ -56,7 +56,7 @@ function Login() {
             </div>
           </div>
           <button
-            className="text-lg w-full pt-4 pb-4 mt-10 text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            className="text-lg w-full pt-4 pb-4 mt-10 text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-full px-5 py-2.5 text-center mr-2 mb-2"
             onClick={() => logInWithEmailAndPassword(email, password)}
           >
             Sign in
