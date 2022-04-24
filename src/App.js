05c3304d1d6848dfecec0a5dc10419f6746fe99d
HT-Dat <hotiendat661@gmail.com>
2022-04-25 02:26:18 +0700

    Remove import App.css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
 import PageRoutes from "./routes/PageRoutes";
-import "./App.css";
-
 function App() {
   // eslint-disable-next-line
   const [user, loading, error] = useAuthState(auth);
