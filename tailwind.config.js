0706e66c58238a1b8999a0d0a8862d01d0bae582
HT-Dat <hotiendat661@gmail.com>
2022-05-02 04:04:37 +0700

    Tell tailwind to use new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        karla: ["Karla", "sans-serif"],
+      },
       boxShadow: {
         "3xl": "15px 15px 10px 0px rgba(0, 0, 0, 0.3)",
       },
