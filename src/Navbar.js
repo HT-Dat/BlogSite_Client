ef3e732dacf4f1a9e9f89afb590db7032708e7c8
HT-Dat <hotiendat661@gmail.com>
2022-05-09 03:06:33 +0700

    Add href link corresponding to home section id

--- src/Navbar.js
+++ src/Navbar.js
@@ -47,7 +47,7 @@ function Navbar() {
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-xl md:font-montserrat">
             <li>
               <a
-                href="#"
+                href="#home"
                 className="block py-2 pr-6 pl-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                 aria-current="page"
               >
@@ -56,7 +56,7 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="#focused-things"
                 className="block py-2 pr-6 pl-4 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 focused things
@@ -64,7 +64,7 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="#about-me"
                 className="block py-2 pr-6 pl-4 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 about me
@@ -72,7 +72,7 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="#projects"
                 className="block py-2 pr-6 pl-4 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 projects
