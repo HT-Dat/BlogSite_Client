c341df259ff3f5655d3696dda64c5083b0bd0e2a
HT-Dat <hotiendat661@gmail.com>
2023-03-19 19:19:48 +0700

    Add a fade animation for navbar bg color

--- src/navbar/link-navbar.js
+++ src/navbar/link-navbar.js
@@ -4,7 +4,7 @@ export default function LinkNavbar({ to, text, action, useIsActive }) {
     <NavLink
       to={to}
       className={({ isActive }) =>
-        "block py-2 pr-6 pl-4 text-white hover:bg-amber-500 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" +
+        "block py-2 pr-6 pl-4 text-white hover:bg-amber-500 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 transition-all ease-in duration-150" +
         (isActive && !useIsActive
           ? " underline underline-offset-8 decoration-white "
           : "")
