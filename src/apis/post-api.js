46c494efd4cd58c6cce3f68427bdff33e9e4a49f
HT-Dat <hotiendat661@gmail.com>
2023-03-30 15:41:12 +0700

    Add get post list

--- src/apis/post-api.js
+++ src/apis/post-api.js
@@ -9,6 +9,14 @@ export const PostAPI = {
 
     return response.data;
   },
+  getList: async function () {
+    const response = await api.request({
+      url: `/api/post`,
+      method: "GET",
+    });
+
+    return response.data;
+  },
   savePost: async function (Post, id) {
     const response = await api.request({
       url: `/api/post/${id}`,
