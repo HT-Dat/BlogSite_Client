cdde2cad11f1fd473cfb8ab5a3b875a003eb3de9
HT-Dat <hotiendat661@gmail.com>
2023-04-02 03:31:54 +0700

    Move blog-admin-editor to editor directory

--- src/blogs/blog-admin/posts/editor/blog-admin-editor.js
+++ src/blogs/blog-admin/posts/editor/blog-admin-editor.js
@@ -1,24 +1,28 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import Editor from "ckeditor5-custom-build/build/ckeditor";
+import CustomUploadAdapterPlugin from "./custom-upload-adapter";
 export default function AdminEditor({
   toolbarRef,
   editorData,
   onWriterChange,
 }) {
+  // const editorConfiguration = {
+  //   simpleUpload: {
+  //     // The URL that the images are uploaded to.
+  //     uploadUrl: `${process.env.REACT_APP_BASEAPI_URL}/api/post/upload-editor-image`,
+
+  //     // Enable the XMLHttpRequest.withCredentials property.
+  //     withCredentials: true,
+
+  //     // Headers sent along with the XMLHttpRequest to the upload server.
+  //     headers: {
+  //       "X-CSRF-TOKEN": "CSRF-Token",
+  //       Authorization: `Bearer ${localStorage.getItem("firebase_auth_token")}`,
+  //     },
+  //   },
+  // };
   const editorConfiguration = {
-    simpleUpload: {
-      // The URL that the images are uploaded to.
-      uploadUrl: `${process.env.REACT_APP_BASEAPI_URL}/api/post/upload-editor-image`,
-
-      // Enable the XMLHttpRequest.withCredentials property.
-      withCredentials: true,
-
-      // Headers sent along with the XMLHttpRequest to the upload server.
-      headers: {
-        "X-CSRF-TOKEN": "CSRF-Token",
-        Authorization: `Bearer ${localStorage.getItem("firebase_auth_token")}`,
-      },
-    },
+    extraPlugins: [CustomUploadAdapterPlugin],
   };
   return (
     <div className="w-full h-full">
