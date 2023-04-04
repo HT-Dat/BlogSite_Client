83e971bf454e400937bbb17d689fcb8b629e1a22
HT-Dat <hotiendat661@gmail.com>
2023-04-04 23:35:23 +0700

    Add getListAdmin, getPublic, delete to Post api layer

--- src/apis/post-api.js
+++ src/apis/post-api.js
@@ -3,23 +3,38 @@ import { api } from "./configs/axios-config";
 export const PostAPI = {
   get: async function (id) {
     const response = await api.request({
-      url: `/api/post/${id}`,
+      url: `/api/post/my-content/${id}`,
       method: "GET",
     });
 
     return response.data;
   },
-  getList: async function () {
+  getPublic: async function (permalink) {
     const response = await api.request({
-      url: `/api/post`,
+      url: `/api/post/public/${permalink}`,
       method: "GET",
     });
 
+    return response.data;
+  },
+  getListAdmin: async function () {
+    const response = await api.request({
+      url: `/api/post/my-content`,
+      method: "GET",
+    });
+
+    return response.data;
+  },
+  getListPublic: async function () {
+    const response = await api.request({
+      url: `/api/post/public`,
+      method: "GET",
+    });
     return response.data;
   },
   savePost: async function (Post, id) {
     const response = await api.request({
-      url: `/api/post/${id}`,
+      url: `/api/post/my-content/${id}`,
       method: "PUT",
       data: Post,
     });
@@ -27,16 +42,23 @@ export const PostAPI = {
   },
   newPost: async function () {
     const response = await api.request({
-      url: `/api/post`,
+      url: `/api/post/my-content`,
       method: "POST",
     });
     return response.data;
   },
   uploadImg: async function (file) {
     const response = await api.request({
-      url: `/api/post/upload-editor-image`,
+      url: `/api/post/my-content/upload-editor-image`,
       method: "POST",
       data: file,
+    });
+    return response;
+  },
+  delete: async function (id) {
+    const response = await api.request({
+      url: `/api/post/my-content/${id}`,
+      method: "DELETE",
     });
     return response;
   },
