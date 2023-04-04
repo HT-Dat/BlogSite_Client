8b86e93706cebe92d73ad0aa47f96af20b0bba6b
HT-Dat <hotiendat661@gmail.com>
2023-04-04 23:42:14 +0700

    Add functionality to publish button, delete button

--- src/blogs/blog-admin/posts/blog-admin-post-card.js
+++ src/blogs/blog-admin/posts/blog-admin-post-card.js
@@ -3,15 +3,42 @@ import { RiDeleteBin2Fill } from "react-icons/ri";
 import { AiFillEye } from "react-icons/ai";
 import { BsFillChatFill, BsFillBarChartFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
-export default function PostCard({ post }) {
+import { useUserAuth } from "../../../utils/auth/firebase-auth-context";
+import { PostAPI } from "../../../apis/post-api";
+export default function PostCard({ post, setTriggerReload }) {
+  const DRAFT = 0;
+  const PENDING = 1;
+  const PUBLISHED = 2;
+
+  const { userFromBackend } = useUserAuth();
+  async function deletePost() {
+    const response = await PostAPI.delete(post.id);
+    setTriggerReload(true);
+  }
+  async function publishPostData() {
+    const postObject = {
+      id: post.id,
+      statusId: userFromBackend.isAdmin ? PUBLISHED : PENDING,
+    };
+    const response = await PostAPI.savePost(postObject, post.id);
+    setTriggerReload(true);
+  }
   return (
-    <Link
-      to={`edit/${post.id}`}
-      className="px-6 py-4 flex bg-white mb-2 hover:shadow-md rounded-md"
-    >
-      <div className="w-20 h-20 border border-slate-200"></div>
+    <div className="px-6 py-4 flex bg-white mb-2 hover:shadow-md rounded-md">
+      <Link
+        to={`edit/${post.id}`}
+        className="w-20 h-20 border border-slate-200 rounded-md"
+      >
+        {post.thumbnailUrl ? (
+          <img src={post.thumbnailUrl} className="object-cover h-full" />
+        ) : (
+          <div className="text-5xl font-serif font-bold h-full flex items-center justify-center text-gray-500">
+            {post.title.charAt(0) || "U"}
+          </div>
+        )}
+      </Link>
       <div className="flex items-center justify-between w-full">
-        <div className="px-5">
+        <Link to={`edit/${post.id}`} className="px-5">
           <div className="text-gray-500 pb-5 text-lg">
             {post.title || "(Untitled)"}
           </div>
@@ -19,11 +46,11 @@ export default function PostCard({ post }) {
             <div className="text-yellow-500">
               {(() => {
                 switch (post.statusId) {
-                  case 0:
+                  case DRAFT:
                     return "Draft";
-                  case 1:
+                  case PENDING:
                     return "Pending";
-                  case 2:
+                  case PUBLISHED:
                     return "Published";
                 }
 
@@ -45,12 +72,20 @@ export default function PostCard({ post }) {
               })()}
             </div>
           </div>
-        </div>
+        </Link>
         <div>
-          <div className="flex justify-end pb-5 text-gray-500">
-            <IoSend className="w-10 h-10 hover:bg-gray-200 p-2 rounded-lg" />
-            <RiDeleteBin2Fill className="w-10 h-10 hover:bg-gray-200 p-2 rounded-lg" />
-            <AiFillEye className="w-10 h-10 hover:bg-gray-200 p-2 rounded-lg" />
+          <div className="flex justify-end pb-5 text-gray-500 cursor-pointer">
+            <IoSend
+              className="w-10 h-10 hover:bg-gray-200 p-2 rounded-lg"
+              onClick={publishPostData}
+            />
+            <RiDeleteBin2Fill
+              className="w-10 h-10 hover:bg-gray-200 p-2 rounded-lg"
+              onClick={deletePost}
+            />
+            <Link to={`edit/${post.id}`}>
+              <AiFillEye className="w-10 h-10 hover:bg-gray-200 p-2 rounded-lg" />
+            </Link>
           </div>
           <div className="flex justify-end">
             <div className="flex items-center">
@@ -64,6 +99,6 @@ export default function PostCard({ post }) {
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
